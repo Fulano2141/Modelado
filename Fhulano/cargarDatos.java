5791e0d91ad8447b763af79a0ea2a0145712063b
Fulano2141 <macuto2142dt@gmail.com>
2020-04-02 18:38:02 -0400

    modified:   Fhulano/cargarDatos.java

--- Fhulano/cargarDatos.java
+++ Fhulano/cargarDatos.java
@@ -145,4 +145,10 @@ public class cargarDatos {
         return data;
     }
 
+    public static double[][] datosPruebas() {
+        double[][] data = { { 7.0 }, { -5.0 }, { 2.0 }, { 4.0 }, { 5.0 }, { 7.0 }, { 2.0 }, { 6.0 }, { 4.0 }, { -2.0 },
+                { -3.0 }, { 11.0 }, { 9.0 }, { 10.0 }, { -4.0 }, { -1.0 }, { 8.0 }, };
+        return data;
+    }
+
 }
