fa79153090d70cc1a0aceeeb46848e1b7fdac7fc
Fulano2141 <macuto2142dt@gmail.com>
2020-04-24 20:42:23 -0400

    modified:   Ejemplos/ReadExcelDemo.java 	modified:   Sample.xlsx

--- Ejemplos/ReadExcelDemo.java
+++ Ejemplos/ReadExcelDemo.java
@@ -11,39 +11,78 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcelDemo {
     public static void main(String[] args) {
+        double[][] datos = leerExcel("Sample.xlsx");
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[0].length; j++) {
+                System.out.print(datos[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static double[][] leerExcel(String archivo) {
+        double[][] aux;
         try {
-            FileInputStream file = new FileInputStream(new File("Sample.xlsx"));
-
-            // Create Workbook instance holding reference to .xlsx file
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-            // Get first/desired sheet from the workbook
-            XSSFSheet sheet = workbook.getSheetAt(0);
-
-            // Iterate through each rows one by one
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-                // For each row, iterate through all the columns
+            FileInputStream fil = new FileInputStream(new File(archivo));
+            XSSFWorkbook workboo = new XSSFWorkbook(fil);
+            XSSFSheet shee = workboo.getSheetAt(0);
+            Iterator<Row> rows = shee.iterator();
+            int i = 0, j = 0;
+            while (rows.hasNext()) {
+                Row row = rows.next();
                 Iterator<Cell> cellIterator = row.cellIterator();
-
                 while (cellIterator.hasNext()) {
+                   
                     Cell cell = cellIterator.next();
-                    // Check the cell type and format accordingly
                     switch (cell.getCellType()) {
                         case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "\t");
+                            // System.out.print(cell.getNumericCellValue() + "\t");
                             break;
                         case Cell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue() + "\t");
+                            // System.out.print(cell.getStringCellValue() + "\t");
                             break;
                     }
+                    j++;
                 }
-                System.out.println("");
+                i++;
+            }
+            fil.close();
+            aux = new double[i][j/i];
+            // aux = null;
+            // System.out.println("i: "+ i + " j: "+ j/i);
+            FileInputStream file = new FileInputStream(new File(archivo));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            i = 0;
+            j = 0;
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                j = 0;
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            aux[i][j] = cell.getNumericCellValue();
+                            // System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            aux[i][j] = Double.parseDouble(cell.getStringCellValue());
+                            // System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                    }
+
+                    j++;
+                }
+                i++;
+                // System.out.println("");
             }
             file.close();
         } catch (Exception e) {
+            aux = null;
             e.printStackTrace();
         }
+        return aux;
     }
 }
